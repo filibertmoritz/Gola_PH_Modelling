329f48346ace27a8718d9b5aae22312c812643e6
filibertmoritz <filibert.heim@posteo.de>
2025-02-10 18:50:36 +0100

    contribution to data prep script

--- scripts/Gola_PH_Modelling_data_prep.R
+++ scripts/Gola_PH_Modelling_data_prep.R
@@ -30,8 +30,10 @@ rename <- dplyr::rename
 # read in data 
 excel_sheets('data/Pygmy Hippopotamus MASTER 2008-2021 08May2023.xlsx')
 pres <- read_excel('data/Pygmy Hippopotamus MASTER 2008-2021 08May2023.xlsx', sheet = 'PYGMY HIPPO MASTER') # these are all presences we have across different projects
+pres_cam <- read.csv('data/combined_PH_data2025_draft2_combined_camera_data.csv') # all presences from the camera trap data
+cam_deploy <- read.csv('data/combined_PH_data2025_draft2_locations_cameras.csv') # all camera deployment locations and times of pres_cam
 
-# format data types properly
+# format data types properly - pres
 str(pres)
 
 pres %>%
@@ -39,8 +41,50 @@ pres %>%
          Time = hms::as_hms(ifelse(is.na(VisitTime_hh_mm_ss), format(VisitDate_dd_mm_yyyy, '%H:%M:%S'), format(VisitTime_hh_mm_ss, '%H:%M:%S')))) %>%
   select(DatasetName, Date, Time, VisitDate_dd_mm_yyyy, VisitTime_hh_mm_ss)
 
-# replace all -9999 with NA 
-pres[pres == -9999] <- NA 
+pres[pres == -9999] <- NA # replace all -9999 with NA 
+
+
+# format data types properly and get rid of unneeded columns - camera deployment data 
+str(cam_deploy)
+cam_deploy <- cam_deploy %>% mutate(project = factor(ProjectIDName), 
+                      country = factor(country), 
+                      River = factor(Riverlocation), 
+                      PlotIDName = factor(PlotIDName),
+                      placename = factor(placename), 
+                      CameraIDName = factor(CameraIDName), 
+                      Deployment = as.Date(DeploymentDate_dd.mm.yyyy, format = '%d/%m/%Y'), 
+                      Collection = as.Date(CollectionDate_dd.mm.yyyy, format = '%d/%m/%Y'), 
+                      Camera_Functioning = factor(if_else(Camera_Functioning == "Camera Functioning", 'functioning', 'failure')), 
+                      habitat = factor(overallhabitat)) %>% 
+  select(project, country, placename, Riverlocation, PlotIDName, CameraIDName, Deployment, Collection, Camera_Functioning, UTM_X_meters, UTM_Y_meters, habitat)
+
+# format data types properly and get rid of uneeded columns - camera presence data 
+str(pres_cam) # this looks rather scattered, thus do it separately for each data set 
+pres_cam <- pres_cam %>% mutate(project = factor(project), 
+                    country = factor(country), 
+                    placename = factor(placename), 
+                    deploymentID = factor(deploymentID), 
+                    CameraIDName = factor(CameraIDName), 
+                    Obs_Date = as.Date(date,format = '%d/%m/%Y'), 
+                    UTM_X_meters = x_coord, UTM_Y_meters = y_coord, habitat = factor(habitat)) %>% 
+  select(project, country, placename, deploymentID, CameraIDName, Obs_Date, time, UTM_X_meters, UTM_Y_meters, count, notes)
+
+# take the different data sources and try to identical link columns to each other for a join 
+
+# Basel Zoo data
+pres_cam %>% filter(project == 'Basel Zoo PygmyHippo 2018-2020')
+cam_deploy %>% filter(project == 'Basel Zoo PygmyHippo 2018-2020') # its possible to join the data via CameraIDName or coordinates
+
+# ARTP data - project names are different in both tables 
+pres_cam %>% filter(project == 'REDD_ARTP_PygmyHippo 2013-2014') %>% select(-notes)
+cam_deploy %>% filter(project == 'REDD PygmyHippo 2013-2014') # only join via coordinates possible 
+
+
+
+
+
+
+#
 
 
 
@@ -56,6 +100,25 @@ pres[pres == -9999] <- NA
 
 
 
+unique(pres_cam$placename)
+
+unique(cam_deploy$ProjectIDName)
+
+
+# data from REDD_ARTP_PygmyHippo 2013-2014 cannot be matched with deployment data because no site IDs exist, probs a spatial join is needed 
+
+# DarwinMorro river data and IWT_CF has no spatial location of deployments 
+
+# visualisize the different deployment periods 
+cam_deploy %>% 
+  mutate(ID = paste0(PlotIDName, '_', CameraIDName)) %>%
+  ggplot(aes(y = ID)) +
+  geom_segment(aes(x = Deployment, xend = Collection, yend = ID), linewidth = 1, color = 'blue') 
+
+# visualise the diffferent locations of various monitoring schemes
+par(mfrow = c(2,1))
+plot(data = cam_deploy, x = cam_deploy$UTM_X_meters, y = cam_deploy$UTM_Y_meters, add = T)
+plot(data = pres_cam, x = pres_cam$x_coord, y = pres_cam$y_coord, col = 'red')
 
 
 ################################################################################
